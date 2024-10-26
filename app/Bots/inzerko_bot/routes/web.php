f6740ec4f740c4c72f6700b161f11a94e6691605
romanlazko <romanlazko@gmail.com>
2024-10-26 16:11:55 +0200

    Создание объявления через бота

--- app/Bots/inzerko_bot/routes/web.php
+++ app/Bots/inzerko_bot/routes/web.php
@@ -18,7 +18,7 @@ use App\Bots\inzerko_bot\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::middleware(['web'])->name('inzerko_bot.')->prefix('inzerko_bot')->group(function () {
     Route::get('auth', [AuthenticatedSessionController::class, 'auth'])
-        // ->middleware(['signed', 'throttle:6,1'])
+        ->middleware(['signed', 'throttle:6,1'])
         ->name('auth');
 
     Route::middleware(['auth'])->name('announcement.')->prefix('announcement')->group(function () {
