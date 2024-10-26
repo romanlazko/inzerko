1c50b48cfdf7f51f082a00596f83b340d52e5460
romanlazko <romanlazko@gmail.com>
2024-10-26 15:35:00 +0200

    Обновление бота

--- app/Bots/inzerko_bot/routes/web.php
+++ app/Bots/inzerko_bot/routes/web.php
@@ -18,7 +18,7 @@ use App\Bots\inzerko_bot\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::middleware(['web'])->name('inzerko_bot.')->prefix('inzerko_bot')->group(function () {
     Route::get('auth', [AuthenticatedSessionController::class, 'auth'])
-        ->middleware(['signed', 'throttle:6,1'])
+        ->middleware(['signed'])
         ->name('auth');
     Route::middleware(['auth'])->name('announcement.')->prefix('announcement')->group(function () {
         Route::get('/create', [AnnouncementController::class, 'create'])
