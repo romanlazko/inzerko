aa96609f4e14cf3bb0556f043711c8987f83a502
romanlazko <romanlazko@gmail.com>
2024-11-08 15:58:39 +0100

    Небольшое обновление дизайна

--- resources/views/profile/partials/update-communication-information-form.blade.php
+++ resources/views/profile/partials/update-communication-information-form.blade.php
@@ -17,8 +17,8 @@
             <x-form.label :value="__('profile.update_communication_information_form.contact_information')" :required="true"/>
 
             <div class="w-full items-center p-1 border border-gray-300 rounded-lg mt-1 space-y-1">
-                <x-form.label for="contact_phone_visible" class="items-center grid grid-cols-2 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
-                    <div class="flex space-x-1">
+                <x-form.label for="contact_phone_visible" class="items-center grid grid-cols-4 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
+                    <div class="flex space-x-1 col-span-3">
                         <x-heroicon-s-phone-arrow-down-left class="size-5 text-indigo-500"/>
                         <span class="font-normal">
                             {{ __('profile.update_communication_information_form.contact_phone') }}
@@ -28,12 +28,12 @@
                     <x-form.checkbox 
                         id="contact_phone_visible" 
                         name="communication[contact_phone][visible]" 
-                        class="peer/phone ml-auto" 
+                        class="peer/phone ml-auto col-span-1" 
                         value="1" 
                         :checked="old('communication.contact_phone.visible', $user->communication?->contact_phone?->visible ?? false)"
                     />
     
-                    <div class="hidden peer-checked/phone:block col-span-2 space-y-2">
+                    <div class="hidden peer-checked/phone:block col-span-full space-y-2">
                         <x-form.label 
                             for="contact_phone" 
                             :value="__('profile.update_communication_information_form.contact_phone_hint')" 
@@ -59,8 +59,8 @@
         
                 <hr class="mx-2">
     
-                <x-form.label for="telegram_visible" class="items-center grid grid-cols-2 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
-                    <div class="flex space-x-1">
+                <x-form.label for="telegram_visible" class="items-center grid grid-cols-4 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
+                    <div class="flex space-x-1 col-span-3">
                         <x-fab-telegram class="size-5 text-blue-500"/>
                         <span class="font-normal">
                             {{ __('Telegram') }}
@@ -70,12 +70,12 @@
                     <x-form.checkbox 
                         id="telegram_visible" 
                         name="communication[telegram][visible]" 
-                        class="peer/telegram ml-auto" 
+                        class="peer/telegram ml-auto col-span-1" 
                         value="1" 
                         :checked="old('communication.telegram.visible', $user->communication?->telegram?->visible ?? false)"
                     />
     
-                    <div class="hidden peer-checked/telegram:block col-span-2 space-y-2">
+                    <div class="hidden peer-checked/telegram:block col-span-full space-y-2">
                         <x-form.label 
                             for="telegram_phone" 
                             :value="__('profile.update_communication_information_form.telegram_phone_hint')" 
@@ -101,8 +101,8 @@
         
                 <hr class="mx-2">
                 
-                <x-form.label for="whatsapp_visible" class="items-center grid grid-cols-2 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
-                    <div class="flex space-x-1">
+                <x-form.label for="whatsapp_visible" class="items-center grid grid-cols-4 gap-2 w-full justify-between hover:bg-gray-50 p-3 rounded-lg">
+                    <div class="flex space-x-1 col-span-3">
                         <x-fab-whatsapp-square class="size-5 text-green-500"/>
                         <span class="font-normal">
                             {{ __('WhatsApp') }}
@@ -112,12 +112,12 @@
                     <x-form.checkbox 
                         id="whatsapp_visible" 
                         name="communication[whatsapp][visible]" 
-                        class="peer/whatsapp ml-auto" 
+                        class="peer/whatsapp ml-auto col-span-1" 
                         value="1" 
                         :checked="old('communication.whatsapp.visible', $user->communication?->whatsapp?->visible ?? false)"
                     />
     
-                    <div class="hidden peer-checked/whatsapp:block col-span-2 space-y-2">
+                    <div class="hidden peer-checked/whatsapp:block col-span-full space-y-2">
                         <x-form.label 
                             for="phone"
                             :value="__('profile.update_communication_information_form.whatsapp_phone_hint')" 
