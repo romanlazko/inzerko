93545500293dfd058c3dd61b464e3b1179ec6c4e
romanlazko <romanlazko@gmail.com>
2025-03-16 20:57:43 +0100

    Добавление ошибки если нет никнейма телеграм

--- resources/views/profile/partials/update-telegram-information-form.blade.php
+++ resources/views/profile/partials/update-telegram-information-form.blade.php
@@ -55,9 +55,9 @@
         </div>
         @if (! auth()->user()->chat->username)
             <div class="p-4 bg-red-600 text-white rounded-2xl max-w-xl mt-6">
-                <div class="text-sm">
+                <a class="text-sm text-blue-500 hover:underline" href="https://telegra.ph/Kak-ustanovit-nik-v-Telegrame-03-16">
                     {{ __('profile.update_telegram_information_form.username_required') }}
-                </div>
+                </a>
             </div>
         @endif
     @endif
